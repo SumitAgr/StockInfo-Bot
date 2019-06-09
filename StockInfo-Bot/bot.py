f8c0e6af02e1ea32bc4b72408489e91fb824241b
Sumit Agrawal <sumit.r.agr@gmail.com>
2019-06-09 03:48:58 -0400

    Update bot.py

--- StockInfo-Bot/bot.py
+++ StockInfo-Bot/bot.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# Importing Reddit library
+# Importing Reddit library and Error handlers
 import praw
+from praw.exceptions import APIException
+import prawcore
 
 # Importing config file with information
 import config
@@ -11,9 +13,13 @@ import pandas as pd
 # Importing requests library for HTTP requests
 import requests
 
-# Importing datetime library for debugging and time library for displaying time
+# Importing datetime library for debugging and time libraries for displaying time
 from datetime import datetime
 import time
+from pytz import timezone
+
+# Adding EST timezone
+est_timezone = timezone('US/Eastern')
 
 # Importing OS library to read/write files
 import os
@@ -48,9 +54,15 @@ def run_bot(bot_login_info, comments_replied_to):
             if stock_comment in comment.body and comment.id not in comments_replied_to and comment.author != config.username :
                 
                 # Defining the url to get data from and creating a DataFrame and then extracting price and company name
-                av_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}".format(symbol, config.av_apikey)
-                data = pd.DataFrame(requests.get(av_url).json()['Time Series (Daily)']).T
-                price = data['4. close'][0]
+                try:
+                    av_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}".format(symbol, config.av_apikey)
+                    data = pd.DataFrame(requests.get(av_url).json()['Time Series (Daily)']).T
+                    price = data['4. close'][0]
+                except:
+                    print("Too many API Calls! Sleeping for a minute")
+                    time.sleep(60)
+                    pass
+                
                 company_name = nasdaq.loc[nasdaq['Symbol'] == symbol, 'Company Name'].iloc[0]
                 
                 bc_url = "https://marketdata.websol.barchart.com/getQuote.csv?apikey={}&symbols={}&fields=fiftyTwoWkHigh%2CfiftyTwoWkHighDate%2CfiftyTwoWkLow%2CfiftyTwoWkLowDate".format(config.bc_apikey, symbol)
@@ -65,11 +77,13 @@ def run_bot(bot_login_info, comments_replied_to):
                 # Writing the comment.id to the txt file
                 with open ("replied_comments.txt", "a") as f:
                     f.write(comment.id + "\n")
+                    
+                est_time = datetime.now(est_timezone)
                 
                 # Defining the variables to show in our comment reply
-                stock_info = "The last price for {} (Nasdaq:*{}*) was **${:.2f}**".format(company_name, symbol, float(price))
+                stock_info = "The last price for {} (Nasdaq: {}) was **${:.2f}**".format(company_name, symbol, float(price))
                 high_low_info = "\n\n The 52 week high is **${}** and 52 week low is **${}**".format(fiftyTwoWkHigh, fiftyTwoWkLow)
-                time_info = " (as of {})".format(time.strftime("%I:%M%p on %b %d, %Y"))
+                time_info = " (as of {})".format(est_time.strftime("%I:%M %p EST on %b %d, %Y"))
                 bot_info = "\n\n ^^I ^^am ^^a ^^new ^^bot ^^and ^^I'm ^^still ^^improving, ^^you ^^can ^^provide ^^feedback ^^by ^^DMing ^^me ^^your ^^suggestions!"
                 
                 # Replying to the comment on reddit
@@ -100,5 +114,12 @@ comments_replied_to = get_replied_comments()
 
 # While loop to continuosly run the run_bot function
 while True:
-    print("\n", datetime.now())
-    run_bot(bot_login(), comments_replied_to)
+    try:
+        print("\n", datetime.now())
+        run_bot(bot_login(), comments_replied_to)
+    except APIException:
+        print("PRAW API Exception was raised! Sleeping for 10 minutes")
+        time.sleep(600)
+    except prawcore.exceptions.Forbidden:
+        print("PRAW 403 HTTP RESPONSE occured! Sleeping for 10 minutes")
+        time.sleep(600)
