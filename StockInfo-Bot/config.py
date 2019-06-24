814b6bee0009611d2687800ff825f9baadda0607
Sumit Agrawal <sumit.r.agr@gmail.com>
2019-06-24 04:28:20 -0400

    Added DynamoDB Credentials

--- StockInfo-Bot/config.py
+++ StockInfo-Bot/config.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+# PRAW Credentials
 username = 'insert username'
 password = 'insert password'
 client_id = 'insert client_id'
 client_secret = 'insert client_secret'
 user_agent = 'insert user_agent'
+
+# API Credentials
 av_apikey = 'insert apikey'
 bc_apikey = 'insert apikey'
+
+# Database Credentials
+aws_access_key = 'insert access key here'
+aws_secret_access_key = 'insert secret access key here'
+region_name = 'insert region here'
